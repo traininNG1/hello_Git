3db4e7e8259b31d1853f802ab1a699df5de638e9
jesna_training <jesna.shenil@xminds.com>
2024-12-18 16:29:55 +0530

    reservation using multithread

---
+++ seat_reservation.py
@@ -0,0 +1,35 @@
+import threading
+
+total_seat = 50
+
+def reserve_seat(id):
+    global total_seat
+
+    seat_request = int(input(f"{id},How many seats you would like to book? :"))
+    
+    if seat_request <= total_seat:
+        total_seat -= seat_request
+        print(f"Passenger {id}:with {seat_request} seat(s) Booking Successfull!..Seat Left : {total_seat}")
+    else:
+        print(f"Passenger {id} Booking Failed..Not enough seats. seat left : {total_seat}")
+def user_thread(id):
+    reserve_seat(id)
+
+def main():
+    global total_seat
+
+    print(f"welcome to Bus reservation...Total seats available :{total_seat}")
+    num = int(input("Number of users:"))
+
+    threads =[]
+    for id in range(1,num+1):
+        a = threading.Thread(target=user_thread,args=(id,))
+        threads.append(a)
+        a.start()
+
+    for a in threads:
+        a.join()
+
+    print("Booking completed")
+if __name__ == "__main__":
+    main()
